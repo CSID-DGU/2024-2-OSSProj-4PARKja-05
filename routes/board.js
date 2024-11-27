bb5428e7e649fcc1ef7fbddfe17469704afc14a2
parrrk <sohee1422@dongguk.edu>
2024-11-27 21:23:07 +0900

    Add back

---
+++ routes/board.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const { checkAuthMiddleware } = require('../util/auth');
+
+
+
+const { afterUploadImage, uploadPost } = require('../controllers/post');
+const { isLoggedIn, isOwner } = require('../middlewares');
+
+const router = express.Router();
+
+const { getMyBoards } = require('../controllers/board.js');
+
+router.get('/myBoard', checkAuthMiddleware, getMyBoards);
+router.get('/like')
+
+module.exports = router;
